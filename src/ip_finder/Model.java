e0056630e4b9f42b67dba28466097927d5d2220b
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-01-01 14:25:48 +0600

    JDK updated and model updated

--- src/ip_finder/Model.java
+++ src/ip_finder/Model.java
@@ -1,4 +1,35 @@
 package ip_finder;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Model {
+
+    /*
+    * Finds the ip address
+    */
+
+    public void getIP(String url) {
+
+        try {
+            InetAddress inetAddress = InetAddress.getByName(url);
+            String ip = inetAddress.getHostAddress();
+        }
+        catch (UnknownHostException e) {
+
+        }
+    }
+
+
+
+    /*
+    * Displays the IP address on a dialog box
+    */
+
+    public void showDialog(String result) {
+
+
+
+    }
+
 }
